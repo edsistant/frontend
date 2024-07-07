da93c22bd2ffdca75644a0fa2cbd50d7c30f31db
Sammy Shear <sammyshear1@gmail.com>
2024-07-06 20:31:47 -0400

    Basic supabase auth

---
+++ pages/login.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const supabase = useSupabaseClient();
+const email = ref("");
+
+const signInWithOtp = async () => {
+  const { error } = await supabase.auth.signInWithOtp({
+    email: email.value,
+    options: {
+      emailRedirectTo: "http://localhost/confirm"
+    }
+  });
+  if (error) {
+    console.log(error)
+  }
+}
+</script>
+
+<template>
+  <div>
+    <button @click="signInWithOtp">
+      Sign in With Email
+    </button>
+    <input type="email" v-model="email" />
+  </div>
+</template>
